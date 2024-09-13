4727b1e63e3537a39a2c141042996f1f5f61f20e
Vaaiibhav Vaidya <vaaiibhav@live.com>
2024-09-13 19:12:46 +0530

    Update constants.js

--- Utils/constants.js
+++ Utils/constants.js
@@ -1,5 +1,5 @@
-// export const serverUrl = "http://funrep.live:5000/";
+export const serverUrl = "http://funrep.live:5000/";
 // export const serverUrl = 'http://localhost:5000/';
-export const serverUrl = "http://192.168.1.2:5000/";
+// export const serverUrl = "http://192.168.1.2:5000/";
 export const gameName = "Ticket_App";
 export const gameAuth = gameName + "_Auth";
